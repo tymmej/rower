183799419634ddc7031997da2e8d0e8ed9d40952
Jakub Tymejczyk <jakub@tymejczyk.pl>
2013-07-06 21:35:49 +0200

    Temp is no longer array. Add few tabs. Reverse sorting in stats table.

--- www/gpx.php
+++ www/gpx.php
@@ -20,7 +20,6 @@ $j=0;
 //create own array
 $trips=array();
 foreach($json['trips'] as $trip) {
-
 	$trips[$i]['name']=$trip['name'];
 	$trips[$i]['date']=str_replace('.gpx', '', $trips[$i]['name']);
 	$trips[$i]['desc']=$trip['desc'];
@@ -47,15 +46,15 @@ $table=array();
 $stats=array();
 
 foreach($trips as $trip) {
-$year=substr($trip['name'], 0, 4);
-$month=substr($trip['name'], 4, 2);
-$stats[$year][$month]['distance']+=$trip['dist'];
-$stats[$year][$month]['time']+=$trip['seconds'];
-$stats[$year]['distance']+=$trip['dist'];
-$stats[$year]['time']+=$trip['seconds'];
-$table[$j].="<tr>\n\t<th colspan=\"4\"><a href=\"gpx.html?file=" . $trip['date'] . "\">"  . $trip['desc'] . "</a> <a href=\"gpx-osm.html?file=" . $trip['date'] . "\">" . $trip['date'] . "</a></th>\n</tr>\n";
-$table[$j].="<tr>\n\t<td>". $trip['dist'] . "km" ."</td>\n\t<td>" . $trip['time_readable'] . "</td>\n\t<td>" . $trip['avg'] . "km/h" . "</td>\n\t<td><a href=\"maps/". $trip['map'] . "\"><img width=200 src=\"maps/mini-". $trip['map'] . "\" /></a></td>\n</tr>\n";
-$j=++$j%3;
+	$year=substr($trip['name'], 0, 4);
+	$month=substr($trip['name'], 4, 2);
+	$stats[$year][$month]['distance']+=$trip['dist'];
+	$stats[$year][$month]['time']+=$trip['seconds'];
+	$stats[$year]['distance']+=$trip['dist'];
+	$stats[$year]['time']+=$trip['seconds'];
+	$table[$j].="<tr>\n\t<th colspan=\"4\"><a href=\"gpx.html?file=" . $trip['date'] . "\">"  . $trip['desc'] . "</a> <a href=\"gpx-osm.html?file=" . $trip['date'] . "\">" . $trip['date'] . "</a></th>\n</tr>\n";
+	$table[$j].="<tr>\n\t<td>". $trip['dist'] . "km" ."</td>\n\t<td>" . $trip['time_readable'] . "</td>\n\t<td>" . $trip['avg'] . "km/h" . "</td>\n\t<td><a href=\"maps/". $trip['map'] . "\"><img width=200 src=\"maps/mini-". $trip['map'] . "\" /></a></td>\n</tr>\n";
+	$j=++$j%3;
 }
 
 $j=0;
@@ -83,12 +82,11 @@ for($k=0; $k<$j; $k++) {
 }
 echo "</tr>\n";
 
-$temp=array();
-
-for($i=1; $i<=12; $i++) {
+//create stats
+for($i=12; $i>=1; $i--) {
 	$empty=1;
-	$temp[$i]="";
-	$temp[$i].="<tr><td>" . $months[$i] . "</td>";
+	$temp="";
+	$temp.="<tr><td>" . $months[$i] . "</td>";
 	$i_zero=sprintf("%02d",$i);
 	for($k=0; $k<$j; $k++) {
 		if($stats[$years[$k]][$i_zero]['distance']!="") $empty=0;
@@ -97,17 +95,18 @@ for($i=1; $i<=12; $i++) {
 		$seconds2=(int)($stats[$years[$k]][$i_zero]['time']-$hours*3600-$minutes*60);
 		$time_readable=$hours. "h ". $minutes . "m " . $seconds2 . "s";
 		if($time_readable=="0h 0m 0s") $time_readable="";
-		$temp[$i].="<td>" . $stats[$years[$k]][$i_zero]['distance'];
-		if($stats[$years[$k]][$i_zero]['distance']!="") $temp[$i].=" km";
-		$temp[$i].="</td><td>" . $time_readable . "</td>";
+		$temp.="<td>" . $stats[$years[$k]][$i_zero]['distance'];
+		if($stats[$years[$k]][$i_zero]['distance']!="") $temp.=" km";
+		$temp.="</td><td>" . $time_readable . "</td>";
 	}
-	$temp[$i].="</tr>\n";
-	if($empty==1) $temp[$i]="";
-	echo $temp[$i];
+	$temp.="</tr>\n";
+	if($empty==1) $temp="";
+	echo $temp;
 }
 
 echo "</table></div>";
 
+//print tables
 echo "<div id=\"col1\">\n<table>\n";
 echo $table[0];
 echo "</table></div>";
