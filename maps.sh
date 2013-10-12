89ae6f2a1a9129da4aa3494c68e2a52ecbbc283d
Jakub Tymejczyk <jakub@tymejczyk.pl>
2013-10-12 16:16:57 +0200

    Add maps.sh to mass create maps files from .gpx

---
+++ maps.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+for FILE in gpx/*.gpx
+do
+FILE=`echo $FILE | sed -e 's/gpx\///'`
+viking viking gpx/"$FILE" &
+PID=$!
+MAP=`echo $FILE | sed -e 's/gpx\///' -e 's/gpx/png/'`
+sleep 3
+import -window root -crop 1396x791+202+87 maps/$MAP
+kill $PID
+MAP_MINI="mini-$MAP"
+convert -resize 20% maps/$MAP maps/$MAP_MINI
+done
