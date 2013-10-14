81e8aeee2d6a277d0eb4c2dcdfeaf5337e19c991
Jakub Tymejczyk <jakub@tymejczyk.pl>
2013-10-14 22:39:53 +0200

    Change to firefox

--- maps.sh
+++ maps.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-for FILE in $1
+for FILE in gpx/*.gpx
 do
 FILE=`echo $FILE | sed -e 's/gpx\///'`
-viking viking gpx/"$FILE" &
+URL=`echo $FILE | sed -e 's/.gpx//'`
+firefox --display=:1 gpx-auto.html?file=$URL &
 PID=$!
 MAP=`echo $FILE | sed -e 's/gpx\///' -e 's/gpx/png/'`
-sleep 3
-import -window root -crop 1396x791+202+87 maps/$MAP
+sleep 20
+DISPLAY=:1 import -window root -crop 1550x800+25+50 maps/$MAP
 kill $PID
 MAP_MINI="mini-$MAP"
 convert -resize 20% maps/$MAP maps/$MAP_MINI
