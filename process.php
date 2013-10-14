52c12537330067d08341e9b5c9a056845d26da82
Jakub Tymejczyk <jakub@tymejczyk.pl>
2013-10-14 18:14:09 +0200

    Rewrite to php

--- process.php
+++ process.php
@@ -3,30 +3,128 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <?php
+
 $base_path="/media/a1f63e22-1c18-4ff1-b63c-f4fcda0408eb/www/rower/";
 
-$uploadfile=$base_path . "tmp/" . basename($_FILES['gpx']['name']);
+function haversineDistance($curLat, $curLon, $prevLat, $prevLon) {
+	$earthMeanRadius=6371000;
 
+	$curLat=deg2rad($curLat);
+	$curLon=deg2rad($curLon);
+	$prevLat=deg2rad($prevLat);
+	$prevLon=deg2rad($prevLon);
+
+	$latDiff=$curLat-$prevLat;
+	$lonDiff=$curLon-$prevLon;
+
+//check wiki for equation
+	return 2*$earthMeanRadius*asin(sqrt(pow(sin($latDiff/2), 2)+cos($prevLat)*cos($curLat)*pow(sin($lonDiff/2), 2)));
+}
+
+function timeDiff($curTime, $prevTime) {
+	$curTime=strtotime($curTime);
+	$prevTime=strtotime($prevTime);
+
+	$diff=$curTime-$prevTime;
+
+//if difference more than 20s, set 5s (pause, eg. ice creams)
+	if($diff > 20) {
+		$diff=5;
+	}
+
+	return $diff;
+}
+
+//filename of new gpx
+$file=basename($_FILES['gpx']['name']);
+
+//move file to tmp folder
+$uploadfile=$base_path . "tmp/" . $file;
 if (move_uploaded_file($_FILES['gpx']['tmp_name'], $uploadfile)) {
-	$xml = XMLReader::open($uploadfile);
+//check if file valid by using xml checks
+	$xml=XMLReader::open($uploadfile);
 	$xml->setParserProperty(XMLReader::VALIDATE, true);
-	if($xml->isValid()){
+	if($xml->isValid()) {
 		$status=1;
 		$desc=preg_replace("/[^A-Za-z0-9ążśźęćńółĄŻŚŹĘĆŃÓŁ-]/u", "", $_POST['desc']);
-        	$cmd=$base_path . "process.sh " . basename($_FILES['gpx']['name']) . " " . $desc;
-		exec($cmd);
+		rename($uploadfile, $base_path . $file);
 	}
 	else{
 		unlink($uploadfile);
+		$status=0;
 	}
 }
 else {
-	$status=0;
+        $status=0;
 }
 
+//if no error, continue
+if($status){
+//load gpx file
+$text=file_get_contents($file);
+
+//check if gpx contains name tag, if not: add
+if(!preg_match("/<name>/", $text)) {
+	$text = preg_replace("/<trk>/", "<trk><name>" . $desc . "</name>", $text);
+	file_put_contents($file, $text);
+}
+
+//load gpx file to SimpleXml
+$gpx=simplexml_load_file($file);
+
+$distance=0;
+$time=0;
+
+//calculate distance and time
+$isFirst=true;
+foreach ($gpx->trk->trkseg->trkpt as $pt) {
+	if($isFirst) {
+		$cur=(array)$pt;
+	        $cur['lat']=$cur['@attributes']['lat'];
+	        $cur['lon']=$cur['@attributes']['lon'];
+		$prev=$cur;
+		$isFirst=false;
+		continue;
+	}
+	$cur=(array)$pt;
+	$cur['lat']=$cur['@attributes']['lat'];
+	$cur['lon']=$cur['@attributes']['lon'];
+
+	$distance+=haversineDistance($cur['lat'], $cur['lon'], $prev['lat'], $prev['lon']);
+	$time+=timeDiff($cur['time'], $prev['time']);
+
+	$prev=$cur;
+}
+$distance=round($distance/1000, 2);
+
+//read gpx.json
+$filename=$file;
+$file=file_get_contents("gpx.json");
+$json=json_decode($file, true);
+
+//create new trip
+$new_trip=array();
+$new_trip['name']=$filename;
+$new_trip['desc']=$desc;
+$new_trip['dist']=$distance;
+$new_trip['time']=floor($time/60) . ":" . $time%60;
+$new_trip['tags']="";
+
+//push to json
+array_push($json['trips'], $new_trip);
+
+//write data
+file_put_contents("gpx.json", jsone_encode($json));
+
+//create screenshot
+$cmd=$base_path . "process.sh " . basename($_FILES['gpx']['name']);
+exec($cmd);
+}
+
+//ouput html
 echo "<head>
-        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />
-        <title>Rower</title>";
+<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />
+<title>Rower</title>";
 if($status){
 	echo "<meta http-equiv=\"Refresh\" content=\"3; url=gpx.php\"";
 }
@@ -41,7 +139,6 @@ else {
 	echo "Błąd";
 }
 
+echo "</body>
+</html>"
 ?>
-
-</body>
-</html>
