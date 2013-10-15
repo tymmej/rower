2aeaaad9ea070f541ab2c7bc64ff6b90b7388b35
Jakub Tymejczyk <jakub@tymejczyk.pl>
2013-10-15 08:31:55 +0200

    run and kill firefox as root, not php

--- process.sh
+++ process.sh
@@ -7,14 +7,14 @@ FILE="$1"
 
 #create screenshot
 URL=`echo $FILE | sed -e 's/.gpx//'`
-firefox --display=:1 gpx-auto.html?file=$URL &
+sudo -u root firefox --display=:1 gpx-auto.html?file=$URL &
 PID=$!
 MAP=`echo $FILE | sed -e 's/gpx\///' -e 's/gpx/png/'`
 sleep 18
 DISPLAY=:1 import -window root -crop 1550x800+25+50 maps/$MAP
 sleep 2
 DISPLAY=:1 import -window root -crop 1550x800+25+50 maps/$MAP
-kill $PID
+sudo -u root kill $PID
 
 MAP_MINI="mini-$MAP"
 convert -resize 20% maps/$MAP maps/$MAP_MINI
