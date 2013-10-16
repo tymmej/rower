ac03a2c00b781bbc4a6e267d5bb9040212f92448
Jakub Tymejczyk <jakub@tymejczyk.pl>
2013-10-16 14:42:05 +0200

    Unify {,auto}process.php

--- autocreate.sh
+++ autocreate.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
+BASE_PATH="/media/a1f63e22-1c18-4ff1-b63c-f4fcda0408eb/www/rower/"
+cd $BASE_PATH
+
 for FILE in gpx/*.gpx
 do
-FILE=`echo $FILE | sed -e 's/gpx\///'`
-NUMBER=`grep -n $FILE new.json | awk -F: '{print $1}'`
-NUMBER=$(($NUMBER + 1))
+	FILE=`echo $FILE | sed -e 's/gpx\///'`
+	NUMBER=`grep -n $FILE new.json | awk -F: '{print $1}'`
+	NUMBER=$(($NUMBER + 1))
 
-if [ $NUMBER -ne 1 ]; then
-echo $NUMBER
-DESC=`sed -n "${NUMBER}{p;q;}" new.json`
-php autoprocess.php $FILE "$DESC"
-fi
+	#if number equals 1 number was 0 or unset->no record in new.json
+	if [ $NUMBER -ne 1 ]; then
+		echo $NUMBER
+		DESC=`sed -n "${NUMBER}{p;q;}" new.json`
+		php process.php $FILE "$DESC"
+		./process.sh $FILE
+	fi
 done
--- process.php
+++ process.php
@@ -1,7 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
 <?php
 
 $base_path="/media/a1f63e22-1c18-4ff1-b63c-f4fcda0408eb/www/rower/";
@@ -36,110 +32,130 @@ function timeDiff($curTime, $prevTime) {
 }
 
 //filename of new gpx
-$filename=basename($_FILES['gpx']['name']);
-$file=$base_path . "gpx/" . $filename;
-//move file to tmp folder
-$uploadfile=$base_path . "tmp/" . $filename;
-if (move_uploaded_file($_FILES['gpx']['tmp_name'], $uploadfile)) {
-//check if file valid by using xml checks
-	$xml=XMLReader::open($uploadfile);
-	$xml->setParserProperty(XMLReader::VALIDATE, true);
-	if($xml->isValid()) {
-		$status=1;
-		$desc=preg_replace("/[^A-Za-z0-9ążśźęćńółĄŻŚŹĘĆŃÓŁ-]/u", "", $_POST['desc']);
-		rename($uploadfile, $file);
-	}
-	else{
-		unlink($uploadfile);
-		$status=0;
-	}
+if(isset($_FILES['gpx']['name'])){
+	$filename=basename($_FILES['gpx']['name']);
+	$desc=preg_replace("/[^A-Za-z0-9ążśźęćńółĄŻŚŹĘĆŃÓŁ-]/u", "", $_POST['desc']);
+	//1-upload; 2-autoprocess
+	$mode=1;
 }
 else {
-        $status=0;
+	$filename=$argv[1];
+	$desc=$argv[2];
+	$mode=2;
+}
+
+//move file to tmp folder
+if($mode==1){
+	$uploadfile=$base_path . "tmp/" . $filename;
+	if (move_uploaded_file($_FILES['gpx']['tmp_name'], $uploadfile)) {
+		//check if file valid by using xml checks
+		$xml=XMLReader::open($uploadfile);
+		$xml->setParserProperty(XMLReader::VALIDATE, true);
+		if($xml->isValid()) {
+			$status=1;
+			rename($uploadfile, $file);
+		}
+		else{
+			unlink($uploadfile);
+			$status=0;
+		}
+	}
+	else {
+			$status=0;
+	}
 }
 
 //if no error, continue
 if($status){
-//load gpx file
-$text=file_get_contents($file);
+	//load gpx file
+	$file=$base_path . "gpx/" . $filename;
+	$text=file_get_contents($file);
 
-//check if gpx contains name tag, if not: add
-if(!preg_match("/<name>/", $text)) {
-	$text = preg_replace("/<trk>/", "<trk><name>" . $desc . "</name>", $text);
-	file_put_contents($file, $text);
-}
-
-//load gpx file to SimpleXml
-$gpx=simplexml_load_file($file);
-
-$distance=0;
-$time=0;
-
-//calculate distance and time
-foreach ($gpx->trk->trkseg as $trkseg) {
-	$isFirst=true;
-	foreach ($trkseg->trkpt as $pt) {
-		if($isFirst) {
-			$cur=(array)$pt;
-		        $cur['lat']=$cur['@attributes']['lat'];
-		        $cur['lon']=$cur['@attributes']['lon'];
-			$prev=$cur;
-			$isFirst=false;
-			continue;
-		}
-		$cur=(array)$pt;
-		$cur['lat']=$cur['@attributes']['lat'];
-		$cur['lon']=$cur['@attributes']['lon'];
-
-		$distance+=haversineDistance($cur['lat'], $cur['lon'], $prev['lat'], $prev['lon']);
-		$time+=timeDiff($cur['time'], $prev['time']);
-
-		$prev=$cur;
+	//check if gpx contains name tag, if not: add
+	if(!preg_match("/<name>/", $text)) {
+		$text = preg_replace("/<trk>/", "<trk><name>" . $desc . "</name>", $text);
+		file_put_contents($file, $text);
 	}
+
+	//load gpx file to SimpleXml
+	$gpx=simplexml_load_file($file);
+
+	$distance=0;
+	$time=0;
+
+	//calculate distance and time
+	foreach ($gpx->trk->trkseg as $trkseg) {
+		$isFirst=true;
+		foreach ($trkseg->trkpt as $pt) {
+			if($isFirst) {
+				$cur=(array)$pt;
+					$cur['lat']=$cur['@attributes']['lat'];
+					$cur['lon']=$cur['@attributes']['lon'];
+				$prev=$cur;
+				$isFirst=false;
+				continue;
+			}
+			$cur=(array)$pt;
+			$cur['lat']=$cur['@attributes']['lat'];
+			$cur['lon']=$cur['@attributes']['lon'];
+
+			$distance+=haversineDistance($cur['lat'], $cur['lon'], $prev['lat'], $prev['lon']);
+			$time+=timeDiff($cur['time'], $prev['time']);
+
+			$prev=$cur;
+		}
+	}
+	$distance=round($distance/1000, 2);
+
+	//read gpx.json
+	$text=file_get_contents($base_path . "gpx.json");
+	$json=json_decode($text, true);
+
+	//create new trip
+	$new_trip=array();
+	$new_trip['name']=$filename;
+	$new_trip['desc']=$desc;
+	$new_trip['dist']=$distance;
+	$new_trip['time']=floor($time/60) . ":" . $time%60;
+	$new_trip['tags']="";
+
+	//push to json
+	array_push($json['trips'], $new_trip);
+
+	//write data
+	file_put_contents($base_path . "gpx.json", json_encode($json));
+
+	//create screenshot
+	$cmd=$base_path . "process.sh " . $filename;
+	exec($cmd);
 }
-$distance=round($distance/1000, 2);
 
-//read gpx.json
-$text=file_get_contents($base_path . "gpx.json");
-$json=json_decode($text, true);
-
-//create new trip
-$new_trip=array();
-$new_trip['name']=$filename;
-$new_trip['desc']=$desc;
-$new_trip['dist']=$distance;
-$new_trip['time']=floor($time/60) . ":" . $time%60;
-$new_trip['tags']="";
-
-//push to json
-array_push($json['trips'], $new_trip);
-
-//write data
-file_put_contents($base_path . "gpx.json", json_encode($json));
-
-//create screenshot
-$cmd=$base_path . "process.sh " . $filename;
-exec($cmd);
-}
 
 //ouput html
-echo "<head>
+if($mode==1) {
+	echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"
+\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
+<html xmlns=\"http://www.w3.org/1999/xhtml\">
+<head>
 <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />
-<title>Rower</title>";
-if($status){
-	echo "<meta http-equiv=\"Refresh\" content=\"3; url=gpx.php\"";
-}
-echo "<link rel=\"stylesheet\" href=\"gpx.css\" />
-</head>
+<title>Rower</title>
+<link rel=\"stylesheet\" href=\"gpx.css\" />";
+
+	if($status){
+		echo "<meta http-equiv=\"Refresh\" content=\"3; url=gpx.php\"";
+	}
+
+	echo "</head>
 <body>";
 
-if($status){
-	echo "Dodano";
-}
-else {
-	echo "Błąd";
-}
+	if($status){
+		echo "Dodano";
+	}
+	else {
+		echo "Błąd";
+	}
 
-echo "</body>
+	echo "</body>
 </html>"
+}
 ?>
