69c744f24ebab56ac800f4ae987132ae07e874f2
Jakub Tymejczyk <jakub@tymejczyk.pl>
2014-04-26 13:56:37 +0200

    Image of map created by Google Static API, X server&Firefox no longer needed Delete old files (merge.sh, viking)

--- autocreate.sh
+++ autocreate.sh
@@ -3,29 +3,13 @@
 BASE_PATH="/media/a1f63e22-1c18-4ff1-b63c-f4fcda0408eb/www/rower/"
 cd $BASE_PATH
 
-MODE=$1
-STATS=0
-MAPS=0
-
-if [ $MODE -eq 1 -o $MODE -eq 3 ]; then
-	STATS=1
-fi
-
-if [ $MODE -eq 2 -o $MODE -eq 3 ]; then
-	MAPS=1
-fi
-
 cp example/gpx.json.empty gpx.json
 chown php:www-data gpx.json
 
 for FILE in gpx/*.gpx
 do
+	echo $FILE
 	DESC=`grep \<name\> $FILE | sed -e 's/<trk><name>//' -e 's/<\/name>//' -e 's/\s//'`
 	FILE=`echo $FILE | sed -e 's/gpx\///'`
-	if [ $STATS -eq 1 ]; then
-		php process.php $FILE "$DESC"
-	fi
-	if [ $MAPS -eq 1 ]; then
-		./process.sh $FILE
-	fi
+	php process.php $FILE "$DESC"
 done
--- process.php
+++ process.php
@@ -24,6 +24,50 @@ function timeDiff($curTime, $prevTime) {
 	return $curTime-$prevTime;
 }
 
+//https://gist.github.com/abarth500/1477057
+function encodeGPolylineNum($num){
+	$fu = false;
+	if($num < 0){
+		$fu = true;
+	}
+	//STEP2
+	$num = round($num * 100000);
+	//STEP3
+	//$num = decbin($num);
+	//STEP4
+	$num = $num << 1;
+	//STEP5
+	if($fu){
+		$num = ~$num;
+	}
+	//STEP6 - STEP7
+	$num = decbin($num);
+	$n = str_split($num);
+	$num = array();
+	$nn = "";
+	for($c=count($n)-1;$c >= 0;$c--){
+		$nn = $n[$c].$nn;
+		if(strlen($nn) == 5){
+			array_push($num,$nn);
+			$nn = "";
+		}
+	}
+	if(strlen($nn)>0){
+		$nn = str_repeat("0",5 - strlen($nn)).$nn;
+		array_push($num,$nn);
+	}
+	//STEP8 - STEP9 - STEP10 - STEP11
+ 
+	for($c = 0;$c < count($num);$c++){
+		if($c != count($num)-1){
+			$num[$c] = chr(bindec($num[$c]) + 32 + 63);
+		}else{
+			$num[$c] = chr(bindec($num[$c]) + 63);
+		}
+	}
+	return implode("",$num);
+}
+
 //filename of new gpx
 if(isset($_FILES['gpx']['name'])){
 	$filename=basename($_FILES['gpx']['name']);
@@ -122,11 +166,46 @@ if($status){
 	//write data
 	file_put_contents($base_path . "gpx.json", json_encode($json));
 
-	//create screenshot
-	if($mode==1){
-		$cmd=$base_path . "process.sh " . $filename;
-		exec($cmd);
+	//create map as image
+	//https://gist.github.com/abarth500/1477057
+	$i=0;
+	
+	$newgpx=array();
+	foreach ($gpx->trk->trkseg as $trkseg) {
+		foreach ($trkseg->trkpt as $pt) {
+			$cur=(array)$pt;
+			$newgpx[$i]['lat']=$cur['@attributes']['lat'];
+			$newgpx[$i]['lon']=$cur['@attributes']['lon'];
+			$i++;
+		}
 	}
+	
+	$enc = "";
+	$old = true;
+	$skip=(int)$i/50;
+	$i=0;
+	foreach($newgpx as $latlng){
+		if($i%$skip==0){
+			if($old === true){
+				$enc .= encodeGPolylineNum($latlng['lat']).
+						encodeGPolylineNum($latlng['lon']);
+			}else{
+				$enc .= encodeGPolylineNum($latlng['lat'] - $old['lat']).
+						encodeGPolylineNum($latlng['lon'] - $old['lon']);
+			}
+			$old = $latlng;
+		}
+		$i++;
+	}
+	$url="http://maps.googleapis.com/maps/api/staticmap?sensor=false&size=640x320&path=weight:3|color:rend|enc:";
+	$url.=$enc;
+	$urlmini="http://maps.googleapis.com/maps/api/staticmap?sensor=false&size=250x125&path=weight:3|color:rend|enc:";
+	$urlmini.=$enc;
+	$imagename=str_replace('.gpx', '', $filename);
+	$img = $base_path.'maps/'.$imagename.'.png';
+	$imgmini = $base_path.'maps/mini-'.$imagename.'.png';
+	file_put_contents($img, file_get_contents($url));
+	file_put_contents($imgmini, file_get_contents($urlmini));
 }
 
 
